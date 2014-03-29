80a5ac1e774811d40ab36bf43186448c4a129c19
Sergio Roselló Morell <sergio-rosello@hotmail.com>
2014-03-29 20:10:15 +0100

    He arreglado todo el codigo

--- src/js/game.js
+++ src/js/game.js
@@ -1,184 +1,162 @@
 (function () {
-  'use strict';
+    'use strict';
 
-  function Game() {
-    this.player = null;
-    this.enemy = null;
-    this.enemigo = null;
-    this.scoreString = null;
-    this.scoreText = null;
-    //this.TimeString = null;
-    //this.TimeText = null;
-    this.score = null;
-    //this.Time = null;
-    this.milliseconds = 0;
-    this.seconds = 0;
-    this.minutes = 0;
-    this.powerUp =null;
-    this.timer=null;
-    this.lastTime=1000;
+    function Game() {
+        this.player = null;
+        this.enemy = null;
+        this.enemigo = null;
+        this.scoreString = null;
+        this.scoreText = null;
+        //this.TimeString = null;
+        //this.TimeText = null;
+        this.score = null;
+        //this.Time = null;
+        this.milliseconds = 0;
+        this.seconds = 0;
+        this.minutes = 0;
+        this.powerUp = null;
+        this.timer = null;
+        this.lastTime = 1000;
+      
     //this.textStyle = { font: '64px Desyrel', align: 'center'};
-  }
+    }
 
-  Game.prototype = {
-
-    create: function () {
-      var x = this.game.width / 2, y = this.game.height / 2;
-
-      this.player = this.add.sprite(x, y, 'player');
-      this.player.anchor.setTo(0.5, 0.5);
-      this.input.onDown.add(this.onInputDown, this);
-      this.enemy = this.add.group(); 
-        this.enemy.enableBody = true;                   // esta linea junto con la de arriba hacen que los enemigos sigan al personaje principal. 
-      this.enemy.createMultiple(/*0.2 + Math.random()**/6, 'enemy');
-      this.enemy.setAll('outOfBoundsKill', true);
-      this.enemigo = this.add.group();
-        this.enemigo.enableBody = true;                 // esta hace lo mismo que la de arriba pero con enemigos. 
-      this.enemigo.createMultiple(/*0.2 + Math.random()**/6, 'enemigo');
-      this.enemigo.setAll('outOfBoundsKill', true);
-      this.powerUp = this.add.group();
-      this.powerUp.createMultiple(1, 'powerUp');
-      this.scoreString = 'Score : ';
-      this.timer = this.add.text(920, 30, 0 + ':'+ 0, { fontSize: '34px', fill: '#fff' });
-      this.scoreText = this.add.text(10, 30, this.scoreString + this.score, { fontSize: '34px', fill: '#fff' });
-      this.player.body.collideWorldBounds = true;
-      this.physics.startSystem(Phaser.physics.ARCADE);                        // aqui da un error super loco cuando hago que los enemigos sigan al protagonista... ¿Que significa en ARCADE?
-        
-    },
-
-    update: function () 
-      {
-         if (this.input.keyboard.isDown(Phaser.Keyboard.A))
-         {
-          this.player.x -=6;
-          this.physics.overlap(this.player, this.enemy, function(player) { player.kill();}, null, this);
-          this.physics.overlap(this.player, this.enemigo, function(player) { player.kill();}, null, this);
-          //this.enemy.body.velocity.x-=200;
-          //this.physics.moveToObject(this.enemy,this.player,200);                  // Consigue el movimiento de los enemigos 
-         }
-          /*else
-          {
-            this.enemy.setAll('this.body.velocity.x', 0);
-            this.enemy.setAll('this.body.velocity.x', 0);
-
-          }*/
-         if (this.input.keyboard.isDown(Phaser.Keyboard.D))
-         {
-          this.player.x +=6;
-          this.physics.overlap(this.player, this.enemy, function(player) { player.kill();}, null, this);
-          this.physics.overlap(this.player, this.enemigo, function(player) { player.kill();}, null, this);
-          //this.enemy.body.velocity.x-=200;
-          //this.physics.moveToObject(this.enemy,this.player,200);
-         }
-          /*else
-          {
-            this.enemy.setAll('this.body.velocity.x', 0);
-            this.enemy.setAll('this.body.velocity.x', 0);
-
-          }*/
-         
-         if (this.input.keyboard.isDown(Phaser.Keyboard.S))
-         {
-          this.player.y +=6;
-          this.physics.overlap(this.player, this.enemy, function(player) { player.kill();}, null, this);
-          this.physics.overlap(this.player, this.enemigo, function(player) { player.kill();}, null, this);
-          //this.physics.moveToObject(this.enemy,this.player,200);
-         }
-          /*else
-          {
-            this.enemy.setAll('this.body.velocity.x', 0);
-            this.enemy.setAll('this.body.velocity.x', 0);
-
-          }*/
-         
-        if (this.input.keyboard.isDown(Phaser.Keyboard.W))
+    Game.prototype =
         {
-          this.player.y -=6;
-          this.physics.overlap(this.player, this.enemy, function(player) { player.kill();}, null, this);
-          this.physics.overlap(this.player, this.enemigo, function(player) { player.kill();}, null, this);
-          //this.physics.moveToObject(this.enemy,this.player, 200);
-         }
-          /*else
+
+            create: function () {
+                var x = this.game.width / 2, y = this.game.height / 2;
+                this.player = this.add.sprite(x, y, 'player');
+                this.player.anchor.setTo(0.5, 0.5);
+                this.input.onDown.add(this.onInputDown, this);
+                this.enemy = this.add.group();
+                this.enemy.enableBody = true;                   // esta linea junto con la de arriba hacen que los enemigos sigan al personaje principal.
+                this.enemy.createMultiple(/*0.2 + Math.random()**/6, 'enemy');
+                this.enemy.setAll('outOfBoundsKill', true);
+                this.enemigo = this.add.group();
+                this.enemigo.enableBody = true;                 // esta hace lo mismo que la de arriba pero con enemigos.
+                this.enemigo.createMultiple(/*0.2 + Math.random()**/6, 'enemigo');
+                this.enemigo.setAll('outOfBoundsKill', true);
+                this.powerUp = this.add.group();
+                this.powerUp.createMultiple(1, 'powerUp');
+                this.scoreString = 'Score : ';
+                this.timer = this.add.text(920, 30, 0 +' : ' + 0, { fontSize: '34px', fill: '#fff' });
+                this.scoreText = this.add.text(10, 30, this.scoreString + this.score, { fontSize: '34px', fill: '#fff' });
+                this.player.body.collideWorldBounds = true;
+                this.physics.startSystem(Phaser.physics.ARCADE);                        // aqui da un error super loco cuando hago que los enemigos sigan al protagonista... ¿Que significa en ARCADE?
+            },
+            update: function () {
+                if (this.input.keyboard.isDown(Phaser.Keyboard.A)) {
+                    this.player.x -= 6;
+                    this.physics.overlap(this.player, this.enemy, function (player) { player.kill(); }, null, this);
+                    this.physics.overlap(this.player, this.enemigo, function (player) { player.kill(); }, null, this);
+                    //this.enemy.body.velocity.x-=200;
+                    //this.physics.moveToObject(this.enemy, this.player, 200);                  // Consigue el movimiento de los enemigos
+                }
+                /*else
           {
             this.enemy.setAll('this.body.velocity.x', 0);
             this.enemy.setAll('this.body.velocity.x', 0);
-
           }*/
+                if (this.input.keyboard.isDown(Phaser.Keyboard.D)) {
+                    this.player.x += 6;
+                    this.physics.overlap(this.player, this.enemy, function (player) { player.kill();} , null, this);
+                    this.physics.overlap(this.player, this.enemigo, function (player) { player.kill() ; }, null, this);
+                    //this.enemy.body.velocity.x-=200;
+                    //this.physics.moveToObject(this.enemy, this.player, 200);
+                }
+                /*else {
+                    this.enemy.setAll('this.body.velocity.x', 0);
+                    this.enemy.setAll('this.body.velocity.x', 0);
+                }*/
+                if (this.input.keyboard.isDown(Phaser.Keyboard.S)) {
+                    this.player.y += 6;
+                    this.physics.overlap(this.player, this.enemy, function(player) { player.kill() ;}, null, this);
+                    this.physics.overlap(this.player, this.enemigo, function(player) { player.kill();}, null, this);
+                    //this.physics.moveToObject(this.enemy,this.player,200);
+               }
+               /*else {
+                   this.enemy.setAll('this.body.velocity.x', 0);
+                   this.enemy.setAll('this.body.velocity.x', 0);
+                }*/
+               if (this.input.keyboard.isDown(Phaser.Keyboard.W)) {
+                   this.player.y -=6;
+                   this.physics.overlap(this.player, this.enemy, function(player) { player.kill();}, null, this);
+                   this.physics.overlap(this.player, this.enemigo, function(player) { player.kill();}, null, this);
+                   //this.physics.moveToObject(this.enemy,this.player, 200);
+                }
+               /*else {
+                   this.enemy.setAll('this.body.velocity.x', 0);
+                   this.enemy.setAll('this.body.velocity.x', 0);
+                }*/
         
-
-        this.enemyCheck = this.enemy.getFirstExists(false);
-        if (this.enemyCheck)
-        {
-          this.enemyCheck.reset(Math.random()*1024, 1000);
-          this.enemyCheck.body.velocity.y = ((0.5 + Math.random())*-100);
+                this.enemyCheck = this.enemy.getFirstExists(false);
+                if (this.enemyCheck) {
+                    this.enemyCheck.reset(Math.random() * 1024, 1000);
+                    this.enemyCheck.body.velocity.y = ((0.5 + Math.random()) * -100);
           /*if(this.input.keyboard.isDown(Phaser.Keyboard.A)){
             //this.enemyCheck === this.player;
           }*/
           //this.enemyCheck.body.velocity.x = //0//((0.2 + Math.random())*-60 || 80);
-        }
+                }
 
-        this.enemigoCheck = this.enemigo.getFirstExists(false);
-        if (this.enemigoCheck)
-        {
-          this.enemigoCheck.reset(Math.random()*1024, -500);
-          this.enemigoCheck.body.velocity.y = ((0.5 + Math.random())*100);
-          //this.enemigoCheck.body.velocity.x = ((0.5 + Math.random())*60 || -80);
-        }
+                this.enemigoCheck = this.enemigo.getFirstExists(false);
+                if (this.enemigoCheck) {
+                    this.enemigoCheck.reset(Math.random() * 1024, -500);
+                    this.enemigoCheck.body.velocity.y = ((0.5 + Math.random()) * 100);
+                    //this.enemigoCheck.body.velocity.x = ((0.5 + Math.random())*60 || -80);
+                }
 
-        this.powerUpCheck = this.powerUp.getFirstExists(false);
-        if(this.powerUpCheck)
-        {
-          this.powerUpCheck.reset(Math.random()*900,Math.random()* 600);
-          //this.physics.overlap(this.player, this.powerUp, function(powerUp) { powerUp.kill();}, null, this);
-        }
+                this.powerUpCheck = this.powerUp.getFirstExists(false);
+                if (this.powerUpCheck) {
+                    this.powerUpCheck.reset(Math.random() * 900, Math.random() * 600);
+                    //this.physics.overlap(this.player, this.powerUp, function(powerUp) { powerUp.kill();}, null, this);
+                }
 
-        this.physics.overlap(this.powerUp, this.player, function(player,powerUp) { powerUp.kill();}, null, this);
-      
-        this.updateTimer();
-    },
-    
-    updateTimer: function () {
+                this.physics.overlap(this.powerUp, this.player, function (player, powerUp) { powerUp.kill(); }, null, this);
+                this.updateTimer();
+            },
+            updateTimer: function () {
 
-        this.minutes = Math.floor(this.game.time.now / 60000) % 60;
+                this.minutes = Math.floor(this.game.time.now / 60000) % 60;
 
-        this.seconds = Math.floor(this.game.time.now / 1000) % 60;
+                this.seconds = Math.floor(this.game.time.now / 1000) % 60;
 
-        this.milliseconds = Math.floor(this.game.time.now) % 100;
+                this.milliseconds = Math.floor(this.game.time.now) % 100;
 
-        if (this.milliseconds < 10){
-          this.milliseconds = '0' + this.milliseconds;
-        }
-        if (this.seconds < 10){
-          this.seconds = '0' + this.seconds;
-        }
+                if (this.milliseconds < 10) {
+                    this.milliseconds = '0' + this.milliseconds;
+                }
+                if (this.seconds < 10) {
+                    this.seconds = '0' + this.seconds;
+                }
 
-        if (this.minutes < 10){
-          this.minutes = '0' + this.minutes;
-        }
+                if (this.minutes < 10) {
+                    this.minutes = '0' + this.minutes;
+                }
 
-        this.timer.setText(this.minutes + ':'+ this.seconds);
-        //this.input.onDown.addOnce(updateText, this);
+                this.timer.setText(this.minutes + ':' + this.seconds);
+                //this.input.onDown.addOnce(updateText, this);
 
-        /*if (this.seconds ++){
-          this.score += 1;
-          this.scoreText.content = this.scoreString + this.score;
-        }*/
-        if (this.game.time.now > this.lastTime)
-        {
-            this.score += 1;
-            this.scoreText.content = this.scoreString + this.score;
-            this.lastTime = this.game.time.now + 1000;
-        }
-  },
+                /*if (this.seconds ++) {
+                      this.score += 1;
+                      this.scoreText.content = this.scoreString + this.score;
+                  }*/
+                if (this.game.time.now > this.lastTime) {
+                    this.score += 1;
+                    this.scoreText.content = this.scoreString + this.score;
+                    this.lastTime = this.game.time.now + 1000;
+                }
+            },
 
-  onInputDown: function () {
-    this.game.state.start('menu');
-  }
+            onInputDown: function () {
+                this.game.state.start('menu');
+            }
 
-};
+        };
 
-window['minijuego-2'] = window['minijuego-2'] || {};
-window['minijuego-2'].Game = Game;
+    window['minijuego-2'] = window['minijuego-2'] || {};
+    window['minijuego-2'].Game = Game;
 
 }());
 
